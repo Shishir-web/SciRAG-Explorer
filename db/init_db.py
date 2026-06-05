02a7dbcbfbde2fce2bd37a4a2ce106f2195a9e3f
shishir kant upadhyay <shishirkantupadhyay@shishirs-MacBook-Air.local>
2026-06-05 15:45:21 +0530

    fix: init.py

--- db/init_db.py
+++ db/init_db.py
@@ -1,14 +1,20 @@
 import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from db.models import Base
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
+from dotenv import load_dotenv
 load_dotenv()
 
+from sqlalchemy import create_engine
+from db.models import Base
+from db.chunk_models import Chunk  # noqa: F401
+
+
 def init_db():
-    engine = create_engine(os.getenv("POSTGRES__URL"))
+    engine = create_engine(os.environ["POSTGRES_URL"])
     Base.metadata.create_all(engine)
     print("Tables created successfully.")
+
 
 if __name__ == "__main__":
     init_db()
